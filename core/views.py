4a96acee581717f32f7e8511298e02ad96eb8710
Anatoly <a.boronilov@infoculture.ru>
2022-07-16 15:34:01 +0200

    profile backend

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
+import django.contrib.auth.models
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+@method_decorator(login_required, name="dispatch")   
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        context={'profiles':profiles}
+        return render(request, 'profileList.html', context=context)
