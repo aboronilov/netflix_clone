f462d31c7fdac790a5538b0a4bd35a97bdab3c0d
Anatoly <a.boronilov@infoculture.ru>
2022-07-18 10:20:42 +0200

    watch movie backend

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import MovieList
+
 
 app_name = "movie"
 
 urlpatterns = [
-    
+    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie_list")
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.utils.decorators import method_decorator
+from django.views import View
+from core.models import Profile
+from .models import Movie
 
-# Create your views here.
+@method_decorator(login_required, name="dispatch")
+class MovieList(View):
+    def get(self, request, *args, **kwargs):
+        profile_id = kwargs.get("profile_id")
+        if profile_id:
+            profile = Profile.objects.get(uuid=profile_id)
+            if profile in request.user.profiles.all():
+                movies = Movie.objects.filter(age_limit=profile.age_limit)
+                context = {
+                    'movies': movies
+                }
+                return render(request, 'movieList.html', context=context)            
+            return redirect(to="core:profile_list")
+        return render(request, 'movieList.html')
+            
+
