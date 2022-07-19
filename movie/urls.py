39db2be48b490665145a4f3df49564fb999f6700
Anatoly <a.boronilov@infoculture.ru>
2022-07-19 16:13:44 +0200

    movie detail backend

--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import MovieList
+from .views import MovieList, ShowMovieDetail
 
 
 app_name = "movie"
 
 urlpatterns = [
-    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie_list")
+    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie_list"),
+    path('movie/detail/<str:movie_id>/', ShowMovieDetail.as_view(), name="movie_detail"),
 ]
--- movie/views.py
+++ movie/views.py
@@ -9,15 +9,36 @@ from .models import Movie
 class MovieList(View):
     def get(self, request, *args, **kwargs):
         profile_id = kwargs.get("profile_id")
-        if profile_id:
+        try:
             profile = Profile.objects.get(uuid=profile_id)
+        except Profile.DoesNotExist:
+            return redirect(to="core:profile_list")
+        else:    
             if profile in request.user.profiles.all():
                 movies = Movie.objects.filter(age_limit=profile.age_limit)
+                try:
+                    showcase = movies.first()
+                except showcase.DoesNotExist:
+                    pass
                 context = {
-                    'movies': movies
+                    'movies': movies,
+                    'show_case': showcase
                 }
-                return render(request, 'movieList.html', context=context)            
-            return redirect(to="core:profile_list")
+                return render(request, 'movieList.html', context=context)
         return render(request, 'movieList.html')
             
+
+@method_decorator(login_required, name="dispatch")
+class ShowMovieDetail(View):
+    def get(self, request, *args, **kwargs):
+        movie_id = kwargs.get('movie_id')
+        try:
+            movie = Movie.objects.get(uuid=movie_id)
+        except Movie.DoesNotExist:
+            return redirect(to="core:profile_list")
+        else:
+            context = {
+                'movie': movie
+            }
+            return render(request, 'showMovie.html', context=context)
 
